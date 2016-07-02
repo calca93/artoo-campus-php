22b5d5f46d15b7c19b891bbb09029cdd1ebb34c5
calca93 <marco.calca@gmail.com>
2016-07-02 06:59:26 +0000

    mysqli setup, query

--- dbconnection.php
+++ dbconnection.php
@@ -20,23 +20,32 @@
    if ($db->connect_error) {
       die("Connection failed: " . $db->connect_error);
    } 
-   echo "Connected successfully (".$db->host_info.")";
+   echo "Connected successfully (".$db->host_info.")\n\n";
     
    
-   //CREAZIONE TABELLA
-   $sql = "CREATE TABLE MyGuests (
-      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-      firstname VARCHAR(30) NOT NULL,
-      lastname VARCHAR(30) NOT NULL,
-      email VARCHAR(50),
-      reg_date TIMESTAMP
-      )";
    
-   if ($db->query($sql) === TRUE) {
-       echo "Table MyGuests created successfully";
-   } else {
-       echo "Error creating table: " . $conn->error;
-   }
+   $sql ="";
+      
+   $result = $db->query($sql);
+   if($result->num_rows == 0)
+      echo 'not found';
+   else
+      echo 'found';
+   
+   // //CREAZIONE TABELLA
+   // $sql = "CREATE TABLE MyGuests (
+   //    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+   //    firstname VARCHAR(30) NOT NULL,
+   //    lastname VARCHAR(30) NOT NULL,
+   //    email VARCHAR(50),
+   //    reg_date TIMESTAMP
+   //    )";
+   
+   // if ($db->query($sql) === TRUE) {
+   //     echo "Table MyGuests created successfully";
+   // } else {
+   //     echo "Error creating table: " . $conn->error;
+   // }
     
    $db->close();
    
